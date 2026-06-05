9c9f7175980d9e2be1fccd7a2bf21dc1bcc7d625
GAbriel dos Santos ALmeida <gsantosalmeida@outlook.com>
2026-06-04 21:44:51 -0300

    chore: purposely break CI to test failure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
     let a = 10;
     let b = 20;
+    let X = 1;
     let result = add(a, b);
     println!("Hello, world!");
     println!("{} + {} = {}", a, b, result);
 }
 
-fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
+fn add(x: i32, y: i32) -> i32 { x + y }
 
 #[cfg(test)]
 mod tests {
@@ -16,7 +15,7 @@ mod tests {
 
     #[test]
     fn test_add() {
-        assert_eq!(add(2, 3), 5);
+        assert_eq!(add(2, 3), 999);
         assert_eq!(add(-1, 1), 0);
         assert_eq!(add(0, 0), 0);
     }
